6adf4246c8af41c7d2ff286fa463cb34d875d7a1
student <student@FJ-1025>
2026-05-21 17:24:09 +0900

    追加機能追加失敗

--- src/main/java/com/example/demo/controller/TaskController.java
+++ src/main/java/com/example/demo/controller/TaskController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -90,5 +91,15 @@ public class TaskController {
 		model.addAttribute("events", events);
 		model.addAttribute("records", records);
 		return "past";
+	}
+
+	@PostMapping("/past/{id}/delete")
+
+	public String delete(
+			@PathVariable Integer id) {
+
+		exerciseRecordsRepository.deleteById(id);
+
+		return "redirect:/past";
 	}
 }
