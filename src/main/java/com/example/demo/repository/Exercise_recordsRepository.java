efac7d83782761dfd826c696137ee137cf9b04a5
student <student@FJ-1025>
2026-05-25 17:01:08 +0900

    css追加

--- src/main/java/com/example/demo/repository/Exercise_recordsRepository.java
+++ src/main/java/com/example/demo/repository/Exercise_recordsRepository.java
@@ -3,9 +3,14 @@ package com.example.demo.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.demo.entity.Exercise_records;
 
 public interface Exercise_recordsRepository extends JpaRepository<Exercise_records, Integer> {
 	List<Exercise_records> findByUserIdOrderByDateDescIdDesc(Integer userId);
+
+	@Query("SELECT SUM(record.burnCalorie) FROM Exercise_records record")
+	int getTotalBurnCalorie();
+
 }
